4b15f1d8005f56acb2851f325e8dbccd7963b78e
Guust <metzguust@gmail.com>
2025-04-16 23:05:21 +0200

    feat: add homePage navigation button to aboutPage

--- 2-3-4-Exercises/public/about/aboutPage.js
+++ 2-3-4-Exercises/public/about/aboutPage.js
@@ -1,5 +1,11 @@
 import {h} from '/js/src/index.js';
 
-export default (_model) => h('.p2', [
-  h('h1', 'About Page')
+export default (model) => h('.p2', [
+  h('h1', 'About Page'),
+  h('button.btn.btn-primary.m1#NavigateHome', {
+    onclick: () => {
+      console.log('Navigating to Home page');
+      model.router.go('?page=home');
+    }
+  }, [info(), 'Home'])
 ]);
