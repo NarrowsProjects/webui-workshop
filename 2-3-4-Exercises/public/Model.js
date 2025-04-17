6aee8158bfc91994ebe4a579c528f3bb00d3d05e
Guust <metzguust@gmail.com>
2025-04-17 23:01:55 +0200

    feat: implement websocket in Model.js

--- 2-3-4-Exercises/public/Model.js
+++ 2-3-4-Exercises/public/Model.js
@@ -1,4 +1,4 @@
-import {Observable, QueryRouter, Loader, sessionService} from '/js/src/index.js';
+import {Observable, QueryRouter, Loader, sessionService, WebSocketClient} from '/js/src/index.js';
 import Home from './home/Home.js';
 import About from './about/About.js';
 
@@ -11,6 +11,8 @@ export default class Model extends Observable {
    * Load all sub-models and bind event handlers
    */
   home;
+  socket=null;
+  socketNr=null;
   constructor() {
     super();
 
@@ -29,22 +31,45 @@ export default class Model extends Observable {
     this.home.bubbleTo(this); // makes notifications from hom 'visible' to Model.
     this.about = new About();
     this.about.bubbleTo(this);
-    
+    this.#webSocketSetup();
     this.handleLocationChange(); // Init first page
   }
 
+  
+  openNrStream(){
+    if (!this.socket.authed) return "Please wait for authentication.";
+    
+    this.socket.sendMessage({command: 'nr-sender', payload: 'I would like to subscribe to the random number stream'});
+  }
+  
   /**
    * Delegates sub-model actions depending on new location of the page
-   */
+  */
   handleLocationChange() {
     switch (this.router.params.page) {
       case 'home':
         break;
-      case 'about':
-        break;
-      default:
-        this.router.go('?page=home');
-        break;
+        case 'about':
+          break;
+          default:
+            this.router.go('?page=home');
+            break;
+           }
+  }
+   
+  // private functions 
+  #webSocketSetup(){
+    this.socket = new WebSocketClient();
+    // When WebSocketClient._handleMessage emits "authed" socket.authed is set to true and the message is printed
+    this.socket.addListener("authed", _=> console.log("Ready to send messages."));
+    this.socket.addListener("command", message=> this.#messageHandler(message));
+  }
+      
+  #messageHandler(message){
+    if (message.command === "nr-send"){
+      this.socketNr = message.payload.socketNr;
+      
+      super.notify();
     }
   }
 }
