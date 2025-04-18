76ead343e161984f833a634326a362edb9fdd686
Guust <metzguust@gmail.com>
2025-04-18 20:05:24 +0200

    fix: move CI yml file to project root

--- 2-3-4-Exercises/test/mocha-application-service.js
+++ 2-3-4-Exercises/test/mocha-application-service.js
@@ -1,6 +1,6 @@
-const { describe, it } = require('mocha');
-const { expect } = require('chai');
-const { ApplicationService } = require('../lib/ApplicationService.js');
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import { ApplicationService } from '../lib/ApplicationService.js';
 
 describe('ApplicationService', () => {
   describe('getApplicationData', () => {
